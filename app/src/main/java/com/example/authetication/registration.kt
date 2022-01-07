e3b8db10608533b1d05840707212bafc766f36b8
Kim <allanadrian.sacramento@gmail.com>
2022-01-08 01:13:56 +0800

    Midterm Exam : Allan Adrian D. Sacramento

---
+++ app/src/main/java/com/example/authetication/registration.kt
@@ -0,0 +1,12 @@
+package com.example.authetication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class registration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
